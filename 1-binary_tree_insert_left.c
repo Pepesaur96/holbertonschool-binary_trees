4ae2a4b1ecfe44c788ff373acb0b32c7e5ffa4c4
Pepesaur96 <jjvazquez96@gmail.com>
2023-09-12 01:29:47 +0000

    task 1.outputcheck6fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,6 +16,9 @@ binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 	if (parent == NULL)
 		return (NULL);
 
+	if (value == '\0')
+		return (NULL);
+
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = parent->left;
